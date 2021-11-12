27b38ae233e0375e7759e60818924dbae064c4ba
qlcid <qlcid@naver.com>
2021-11-12 11:32:42 +0900

    feat: User Entity 추가 설정 및 Role Entity 추가

--- backend/src/main/java/com/a506/blockai/db/entity/User.java
+++ backend/src/main/java/com/a506/blockai/db/entity/User.java
@@ -5,21 +5,29 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.sql.Date;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "user")
 @Getter
 @Builder
 @DynamicInsert
 @AllArgsConstructor
 @RequiredArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotNull
@@ -32,15 +40,23 @@ public class User {
     private String password;
 
     @NotNull
-    private Date birth;
+    private String birth;
 
     private String phone;
 
-    @NotNull
-    private LocalDateTime created_at;
+    @Column(name = "created_at", nullable = false, updatable = false, insertable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp created_at;
 
-    @NotNull
     private String key;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Certification certification;
@@ -48,4 +64,28 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private DID did;
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
