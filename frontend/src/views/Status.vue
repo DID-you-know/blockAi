a9914fd779900a9f7df4c6bd3bfd9aee6a2684ca
kiung <rldnd22@gmail.com>
2021-11-16 23:13:43 +0900

    feat: 발급완료 페이지 UI 수정

--- frontend/src/views/Status.vue
+++ frontend/src/views/Status.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="status body">
     <Navbar/>
-    <DIDCard/>
+    <DIDCard :name="name" :issuedAt="issuedDate" class="did-card"/>
+    <div class="background">
+      <span class="message">위 사람은 BlockAi가 인증된 신원임을 보장합니다.</span>
+    </div>
   </div>
 </template>
 
 <script>
   import Navbar from '@/components/Navbar'
   import DIDCard from '@/components/DIDCard'
+  import { useStore } from 'vuex'
+  import { computed } from 'vue'
 
 
   export default {
@@ -15,15 +20,58 @@
     components: {
       Navbar,
       DIDCard
+    },
+    setup() {
+      const store = useStore()
+      const name = computed(() => store.state.users.name)
+      const issuedDate = computed(() => {
+        const issuedAt = store.state.users.issuedDate
+        return issuedAt.slice(0, 10).replace(/-/g, '.')
+      })
+
+      return {
+        name,
+        issuedDate
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  @import "@/assets/style/color.scss";
+
+
   .status {
-    padding-top: 7rem;
+    padding-top: 5rem;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
+    overflow: hidden;
+    position: relative;
+    background-color: $light;
+
+    .did-card {
+      z-index: 1;
+    }
+
+    $height: 70vh;
+
+    .background {
+      position: absolute;
+      width: $height * 1.62;
+      height: $height;
+      box-shadow: 1vh 1vh 3vh 1vh rgba($color: #000000, $alpha: 0.15);
+      border-radius: 3vh;
+      transform: translateY(50%);
+
+      .message {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: 2.5vh;
+      }
+    }
   }
 </style>
