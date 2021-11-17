00e76c3f732b5b6f252b209034b49f4129503344
이한울 <dkfghg3@naver.com>
2021-11-18 03:31:32 +0900

    feat: 얼굴, 목소리 파일 조회

--- backend/src/main/java/com/a506/blockai/api/controller/AiController.java
+++ backend/src/main/java/com/a506/blockai/api/controller/AiController.java
@@ -1,7 +1,9 @@
 package com.a506.blockai.api.controller;
 
-import com.a506.blockai.api.dto.request.FaceBiometricsRequest;
-import com.a506.blockai.api.dto.request.VoiceBiometricsRequest;
+import com.a506.blockai.api.dto.request.FaceBiometricRequest;
+import com.a506.blockai.api.dto.request.VoiceBiometricRequest;
+import com.a506.blockai.api.dto.response.FaceBiometricResponse;
+import com.a506.blockai.api.dto.response.VoiceBiometricResponse;
 import com.a506.blockai.api.service.AiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +20,24 @@ public class AiController {
     final private AiService aiService;
 
     @PostMapping("/{voiceId}/voice")
-    public ResponseEntity<?> identify(@PathVariable String voiceId, @RequestBody VoiceBiometricsRequest voiceBiometricsRequest) throws IOException, UnsupportedAudioFileException {
+    public ResponseEntity<?> identify(@PathVariable String voiceId, @RequestBody VoiceBiometricRequest voiceBiometricsRequest) throws IOException, UnsupportedAudioFileException {
         return ResponseEntity.status(200).body(aiService.identifyVoice(voiceBiometricsRequest));
     }
 
     @PostMapping("/{userId}/face")
-    public ResponseEntity<?> identifyFace(@PathVariable String userId, @RequestBody FaceBiometricsRequest faceBiometricsRequest) throws Exception {
+    public ResponseEntity<?> identifyFace(@PathVariable String userId, @RequestBody FaceBiometricRequest faceBiometricsRequest) throws Exception {
         float result = aiService.identifyFace(faceBiometricsRequest);
         return ResponseEntity.status(200).body(result);
     }
+
+    @GetMapping("/users/{userId}/face")
+    public ResponseEntity<FaceBiometricResponse> searchFace(@PathVariable int userId) throws Exception {
+        return ResponseEntity.ok(aiService.getFaceData(userId));
+    }
+
+    @GetMapping("/users/{userId}/voice")
+    public ResponseEntity<VoiceBiometricResponse> searchVoice(@PathVariable int userId) throws Exception {
+        return ResponseEntity.ok(aiService.getVoiceData(userId));
+    }
+
 }
